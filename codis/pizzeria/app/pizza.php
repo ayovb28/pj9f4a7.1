b574aac6a36a215e24e712349d1701681e49e107
ayovb28 <464037.clot@fje.edu>
2025-10-22 20:22:25 +0200

    Versió 1.1 del projecte pizzeria

--- codis/pizzeria/app/pizza.php
+++ codis/pizzeria/app/pizza.php
@@ -1,19 +1,33 @@
-<?php
-	$pvp = 8;
-	if (empty($_GET['ingr'])) {
-		echo "Has demanat només la pizza bàsica. El preu és $pvp € (IVA inclòs)<br>";
-		exit(0);
-	}
-	else {
-		$IVA=21;
-		$pvp=((100+$IVA)/100)*(8+count($_GET['ingr']));
-		echo "Has demanat la pizza bàsica amb:<br> ";
-		foreach ($_GET['ingr'] as $ingredient) {
-			if ($ingredient) {
-				echo "* $ingredient<br>";
-			}
-		}
-		echo "El preu és $pvp € (IVA inclòs)<br>";
-		exit(0);
-	}
-?>
+<html>
+  <head>
+    <meta content="text/html; charset=UTF-8" http-equiv="content-type">
+    <title>RESULTAT</title>
+  </head>
+  <body>
+    <font face="Arial">
+      <?php
+        $pvp = 10;     // preu base
+        $IVA = 21;     // percentatge d'IVA
+        $num_ingr = 0; // inicialitzem la variable
+
+        echo "Has demanat la pizza bàsica amb:<br>";
+
+        // si existeixen ingredients seleccionats
+        if (isset($_GET['ingr']) && is_array($_GET['ingr'])) {
+          foreach($_GET['ingr'] as $valor) {
+            echo "* $valor<br>";
+          }
+          $num_ingr = count($_GET['ingr']);
+        } else {
+          echo "Cap ingredient extra<br>";
+        }
+
+        // càlcul del preu total
+        $pvp_public = ((100 + $IVA) / 100) * ($pvp + $num_ingr);
+
+        echo "<br><b>El preu és $pvp_public € (IVA inclòs)</b><br>";
+      ?>
+    </font>
+  </body>
+</html>
+
