3d4996abb9276ce943c5f5d99c1217c7dd4435d9
ayovb28 <464037.clot@fje.edu>
2025-11-16 13:20:17 +0100

    Versió 1.2 del projecte pizzeria

--- codis/pizzeria/app/pizza.php
+++ codis/pizzeria/app/pizza.php
@@ -1,18 +1,17 @@
 <html>
-  <head>
+  <head> 
     <meta content="text/html; charset=UTF-8" http-equiv="content-type">
     <title>RESULTAT</title>
   </head>
   <body>
     <font face="Arial">
       <?php
-        $pvp = 10;     // preu base
-        $IVA = 21;     // percentatge d'IVA
-        $num_ingr = 0; // inicialitzem la variable
+        $pvp = 10;    
+        $IVA = 21;     
+        $num_ingr = 0; 
 
         echo "Has demanat la pizza bàsica amb:<br>";
 
-        // si existeixen ingredients seleccionats
         if (isset($_GET['ingr']) && is_array($_GET['ingr'])) {
           foreach($_GET['ingr'] as $valor) {
             echo "* $valor<br>";
@@ -22,7 +21,6 @@
           echo "Cap ingredient extra<br>";
         }
 
-        // càlcul del preu total
         $pvp_public = ((100 + $IVA) / 100) * ($pvp + $num_ingr);
 
         echo "<br><b>El preu és $pvp_public € (IVA inclòs)</b><br>";
